f617516d3c60eafe5286c30dc8e457664d0eaac2
liya.zhu <liya.zhu@msxf.com>
2022-04-29 11:36:35 +0800

    优化 Gson 转换器代码，并添加说明

--- tube/src/test/java/com/tube/http/GsonConverterFactory.kt
+++ tube/src/test/java/com/tube/http/GsonConverterFactory.kt
@@ -9,25 +9,36 @@ import java.lang.reflect.Method
 import java.lang.reflect.Type
 
 /**
- * Describe:
+ * Describe: Gson 转换器工厂
  * Created by liya.zhu on 2022/3/25
  */
-class GsonConverterFactory(val gson: Gson = Gson()) : Converter.Factory {
+class GsonConverterFactory(private val gson: Gson = Gson()) : Converter.Factory {
 
     override fun requestBodyConverter(type: Type, method: Method): Converter<*, RequestBody> {
-        return object : Converter<Any, RequestBody> {
-            override fun convert(value: Any): RequestBody {
-                return RequestBody.create(gson.toJson(value, type), ContentType.JSON)
-            }
-        }
+        return RequestBodyConverter(gson, type)
     }
 
     override fun responseBodyConverter(type: Type, method: Method): Converter<ResponseBody, *> {
-        return object : Converter<ResponseBody, Any> {
-            override fun convert(value: ResponseBody): Any {
-                val charset = value.contentType()?.getCharset() ?: Charsets.UTF_8
-                return gson.fromJson(String(value.byteArray(), charset), type)
-            }
+        return ResponseBodyConverter(gson, type)
+    }
+
+    /**
+     * 请求消息体 Gson 转换器
+     */
+    private class RequestBodyConverter(private val gson: Gson, private val type: Type) :
+        Converter<Any, RequestBody> {
+        override fun convert(value: Any) =
+            RequestBody.create(gson.toJson(value, type), ContentType.JSON)
+    }
+
+    /**
+     * 响应消息体 Gson 转换器
+     */
+    private class ResponseBodyConverter(private val gson: Gson, private val type: Type) :
+        Converter<ResponseBody, Any> {
+        override fun convert(value: ResponseBody): Any {
+            val charset = value.contentType()?.getCharset() ?: Charsets.UTF_8
+            return gson.fromJson(String(value.byteArray(), charset), type)
         }
     }
 }
