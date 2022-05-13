dfafca7e4f255da4e4f79a7ed39d548e8ce936b8
liya.zhu <liya.zhu@msxf.com>
2022-05-13 16:57:48 +0800

    修改 Demo 日志 Tag

--- app/src/main/java/com/tube/http/util/MLog.kt
+++ app/src/main/java/com/tube/http/util/MLog.kt
@@ -5,7 +5,7 @@ import android.util.Log
 class MLog private constructor() {
 
     companion object {
-        private const val TAG = "Tube"
+        private const val TAG = "TubeDemo"
 
         fun d(msg: String, tag: String = TAG) {
             Log.d(tag, "$msg,thread:${Thread.currentThread()}")
