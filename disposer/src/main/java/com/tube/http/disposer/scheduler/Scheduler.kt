bdc735a02d6c261082e7ce51eccb1990279e0c17
liya.zhu <liya.zhu@msxf.com>
2022-05-13 10:04:33 +0800

    优化调度器，添加注释说明

--- disposer/src/main/java/com/tube/http/disposer/scheduler/Scheduler.kt
+++ disposer/src/main/java/com/tube/http/disposer/scheduler/Scheduler.kt
@@ -3,32 +3,63 @@ package com.tube.http.disposer.scheduler
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * 调度器
+ */
 abstract class Scheduler {
 
     companion object {
-        fun io() = IoScheduler()
+        /**
+         * 获取主线程调度器
+         */
+        fun main() = CoroutineScheduler(Dispatchers.Main)
+
+        /**
+         * 获取 IO 调度器
+         */
+        fun io() = CoroutineScheduler(Dispatchers.IO)
     }
 
+    /**
+     * 执行
+     */
     protected abstract fun run(block: () -> Unit)
 
+    /**
+     * 取消调度执行
+     */
     abstract fun cancel()
 
+    /**
+     * 安排调度
+     */
     fun schedule(block: () -> Unit) {
         run(block)
     }
 
-    class IoScheduler : Scheduler() {
-
+    /**
+     * 协程调度器
+     */
+    class CoroutineScheduler(coroutineDispatcher: CoroutineDispatcher) : Scheduler() {
+        /**
+         *
+         */
         private val scope = object : CoroutineScope {
-            override val coroutineContext: CoroutineContext = Dispatchers.IO + Job()
+            override val coroutineContext: CoroutineContext = coroutineDispatcher + Job()
         }
 
+        /**
+         * 通过指定协程作用域进行调度执行
+         */
         override fun run(block: () -> Unit) {
             scope.launch {
                 block()
             }
         }
 
+        /**
+         * 取消协程
+         */
         override fun cancel() {
             scope.cancel()
         }
