642dab95036fec0fad18310e2a4043f6962e2a5d
liya.zhu <liya.zhu@msxf.com>
2022-05-24 15:02:01 +0800

    修改 HttpException 异常构造器 添加 OkHttpRelation.md 说明文件

--- lemon/src/main/java/com/lemon/http/HttpException.kt
+++ lemon/src/main/java/com/lemon/http/HttpException.kt
@@ -6,10 +6,14 @@ import com.lemon.http.request.Response
  * Describe: Http 异常统一封装类
  * Created by liya.zhu on 2022/3/15
  */
-class HttpException(message: String) : RuntimeException(message) {
+class HttpException : RuntimeException {
+    
+    constructor(message: String) : super(message)
+
+    constructor(message: String, cause: Throwable) : super(message, cause)
 
     companion object {
-        fun create(msg: String, e: Throwable) = HttpException("$msg: ${e.message}")
+        fun create(msg: String, e: Throwable) = HttpException("$msg: ${e.message}", e)
 
         fun create(response: Response) =
             HttpException("Http ${response.code} error! url:${response.request.url}")
